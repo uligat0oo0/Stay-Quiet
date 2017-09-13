bd40f842c19d27b120406fd6e04e479959e7fcc1
Your Name <abisaher@gmail.com>
2017-09-12 19:37:49 -0500

    v 1.0

---
+++ app/src/main/java/androides/stayquiet/MainActivity.java
@@ -0,0 +1,30 @@
+package androides.stayquiet;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button entrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        entrar = (Button)findViewById(R.id.button4);
+
+        entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
